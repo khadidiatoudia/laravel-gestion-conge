ad87c803cf421f34a8cc16b2a1abf931dd7681e7
Khadija <khadidiatou.dia@etu.ussein.edu.sn>
2026-07-19 11:15:55 +0000

    fix: trust Render proxy + health check sur /up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => App\Http\Middleware\AdminMiddleware::class,
         ]);
+
+        // Render (comme Heroku/Fly.io) place l'app derrière un proxy inverse.
+        // Sans ceci, Symfony peut rejeter le Host/X-Forwarded-* envoyé par le proxy.
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
